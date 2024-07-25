d50a77bad5f1847ee71faf227d3ca0e55dd2ae1d
mozersz <souza.joab@escola.pr.gov.br>
2024-07-25 11:50:45 -0300

    js

---
+++ html/js/login.js
@@ -0,0 +1,7 @@
+const usuario.document.getElementById("imput");
+const senha.document.getElementById("imput");
+if(usuario == "" || senha == "") {
+    alert("Os campos não foram preenchidos");
+} else{
+    alert("funfo porra")
+}
